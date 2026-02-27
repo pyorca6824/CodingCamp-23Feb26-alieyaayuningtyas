cfe4de2cd3919045d6fd34060766cd118dfbc0e7
peeyo <alieyaayuningtyasrr6824@gmail.com>
2026-02-27 21:29:57 +0700

    Delete JS directory

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-// define a simple welcomeMessage function to avoid reference errors
-function welcomeMessage() {
-    console.log("Welcome to Alieya's Portfolio!");
-    // you can update DOM or show alerts here if desired
-}
-
-welcomeMessage();
-
-/// JavaScript code for Alieya's Portfolio Website
